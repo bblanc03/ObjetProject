271efee322ffbe9ad62993c1a3a69347355115c7
Brandon <bblanc030509@gmail.com>
2024-11-07 13:44:10 -0500

    fixed

--- src/model/DVD.java
+++ src/model/DVD.java
@@ -2,4 +2,10 @@ package model;
 
 public class DVD {
 
+	private static final long serialVersionUID = 2380696760516737212L;
+	
+	private StringProperty noDvd;
+	private StringProperty titre;
+	private StringProperty date;
+	private StringProperty auteur;
 }
