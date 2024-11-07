140d154a0eb957bebcb9534d64a071466fb397d3
Brandon <bblanc030509@gmail.com>
2024-11-07 13:41:21 -0500

    DVD

--- src/model/DVD.java
+++ src/model/DVD.java
@@ -1,5 +1,17 @@
 package model;
 
-public class DVD {
+import java.io.Serializable;
 
+import javafx.beans.property.StringProperty;
+
+public class DVD implements Serializable{
+
+	private static final long serialVersionUID = 2380696760516737212L;
+	
+	private StringProperty noDvd;
+	private StringProperty titre;
+	private StringProperty date;
+	private StringProperty auteur;
+	
+	
 }
