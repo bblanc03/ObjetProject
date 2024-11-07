915f0d3c6933834ab177537e3550fef302a5237b
Zacab90 <zacbellerose@gmail.com>
2024-11-07 15:12:17 -0500

    Model Periodique

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module Mediatheque {
 	requires javafx.graphics;
 	requires javafx.fxml;
+	requires javafx.base;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	//opens controller to javafx.fxml;
