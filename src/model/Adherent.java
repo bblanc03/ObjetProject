e76de9e152431d1539a7e7c70435432b9de185a9
Brandon <bblanc030509@gmail.com>
2024-11-09 16:04:52 -0500

    setup of livre.java

--- src/model/Adherent.java
+++ src/model/Adherent.java
@@ -11,5 +11,40 @@ public class Adherent {
 	
 	public Adherent(String nom, String prenom, String adresse, String telephone) {
 		this.nom = new SimpleStringProperty(nom);
+		this.prenom = new SimpleStringProperty(prenom);
+		this.adresse = new SimpleStringProperty(adresse);
+		this.telephone = new SimpleStringProperty(telephone);
+	}
+
+	public StringProperty getNomProperty() {
+		return nom;
+	}
+	
+	public String getNom() {
+		return nom.get();
+	}
+
+	public StringProperty getPrenomProperty() {
+		return prenom;
+	}
+	
+	public String getPrenom() {
+		return prenom.get();
+	}
+
+	public StringProperty getAdresseProperty() {
+		return adresse;
+	}
+	
+	public String getAdresse() {
+		return adresse.get();
+	}
+
+	public StringProperty getTelephoneProperty() {
+		return telephone;
+	}
+	
+	public String getTelephone() {
+		return telephone.get();
 	}
 }
