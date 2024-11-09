93db35b36d7a931cca840322627e9e25740619b1
Brandon <151796239+bblanc03@users.noreply.github.com>
2024-11-09 18:04:51 -0500

    Revert "Brandon"

--- src/model/DVD.java
+++ src/model/DVD.java
@@ -1,43 +1,5 @@
 package model;
 
-import java.io.Serializable;
+public class DVD {
 
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-
-public class DVD implements Serializable{
-
-	private static final long serialVersionUID = 3443381829226820383L;
-	
-	private StringProperty noDVD;
-	private StringProperty titre;
-	private StringProperty date;
-	private StringProperty auteur;
-	
-	public DVD(String noDVD, String titre, String date, String auteur) {
-		this.noDVD = new SimpleStringProperty(noDVD);
-		this.titre = new SimpleStringProperty(titre);
-		this.date = new SimpleStringProperty(date);
-		this.auteur = new SimpleStringProperty(auteur);
-	}
-	
-	public StringProperty getNoDvdProperty() {
-		return noDVD;
-	}
-	
-	public String getNoDVD() {
-		return noDVD.get();
-	}
-	
-	public StringProperty getTitrePropetrty() {
-		return titre;
-	}
-	public StringProperty getDateProperty() {
-		return date;
-	}
-	public StringProperty getAuteurProperty() {
-		return auteur;
-	}
-	
 }
