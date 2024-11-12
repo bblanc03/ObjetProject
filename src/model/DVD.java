510fde9ba6fe9c8c118426ad75100956a7fae5c8
Brandon <151796239+bblanc03@users.noreply.github.com>
2024-11-12 12:17:16 -0500

    Update DVD.java

--- src/model/DVD.java
+++ src/model/DVD.java
@@ -4,14 +4,7 @@ import java.io.Serializable;
 
 import javafx.beans.property.StringProperty;
 
-public class DVD implements Serializable{
-
-	private static final long serialVersionUID = 2380696760516737212L;
-	
-	private StringProperty noDvd;
-	private StringProperty titre;
-	private StringProperty date;
-	private StringProperty auteur;
+public class DVD {
 	
 	
 }
