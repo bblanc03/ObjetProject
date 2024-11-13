417ae8ca461018b3363aa46bac87be394dd0f4a9
Zacab90 <zacbellerose@gmail.com>
2024-11-12 20:07:32 -0500

    DVD

--- src/model/DVD.java
+++ src/model/DVD.java
@@ -1,17 +1,7 @@
 package model;
 
-import java.io.Serializable;
 
-import javafx.beans.property.StringProperty;
+public class DVD{
 
-public class DVD implements Serializable{
-
-	private static final long serialVersionUID = 2380696760516737212L;
-	
-	private StringProperty noDvd;
-	private StringProperty titre;
-	private StringProperty date;
-	private StringProperty auteur;
-	
 	
 }
