6003fe3764bee47bbf93ba6dea00d9946c18061e
Brandon <bblanc030509@gmail.com>
2024-11-18 12:35:08 -0500

    when adding a worker it checks to see if the fields are empty

--- src/controller/AjouterPreposeCOntroller.java
+++ src/controller/AjouterPreposeCOntroller.java
@@ -4,40 +4,70 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import model.Personne;
 
 public class AjouterPreposeCOntroller {
-	
-	
 
 	@FXML
 	TextField tbNom;
-	
+
 	@FXML
 	TextField tbPrenom;
-	
+
 	@FXML
 	TextField tbAdresse;
-	
+
 	@FXML
 	TextField tbTelephone;
 	
 	@FXML
+	TextField tbMotDePasse;
+
+	@FXML
 	Button btnAjouter;
-	
+
 	@FXML
 	private void initialize() {
-	
+
 	}
-	
+
 	@FXML
 	protected void ajouterPrepose(ActionEvent event) {
-		System.out.println("button ajouter pressed");
-		Personne personne = new Personne(tbNom.getText(), tbPrenom.getText(), tbAdresse.getText(), tbTelephone.getText());
-		//Personne.ajouterPersonne();
-		Personne.setObservableList(personne);
-		System.out.println(Personne.getObservableList());
+		Alert alert = new Alert(AlertType.NONE);
+
+		if (tbNom.getText().equals("")) {
+			alert.setAlertType(AlertType.ERROR);
+			alert.setContentText("Vous n'avez pas tapé votre nom");
+			alert.show();
+		} else if (tbPrenom.getText().equals("")) {
+			alert.setAlertType(AlertType.ERROR);
+			alert.setContentText("Vous n'avez pas tapé votre prénom");
+			alert.show();
+		} else if (tbAdresse.getText().equals("")) {
+			alert.setAlertType(AlertType.ERROR);
+			alert.setContentText("Vous n'avez pas tapé votre adresse");
+			alert.show();
+		} else if (tbTelephone.getText().equals("")) {
+			alert.setAlertType(AlertType.ERROR);
+			alert.setContentText("Vous n'avez pas tapé votre téléphone");
+			alert.show();
+		} else if (tbMotDePasse.getText().equals("")) {
+			alert.setAlertType(AlertType.ERROR);
+			alert.setContentText("Vous n'avez pas tapé votre mot de passe");
+			alert.show();
+		} else {
+			System.out.println("button ajouter pressed");
+			Personne personne = new Personne(tbNom.getText(), tbPrenom.getText(), tbAdresse.getText(),
+					tbTelephone.getText());
+			// Personne.ajouterPersonne();
+			Personne.setObservableList(personne);
+			System.out.println(Personne.getObservableList());
+		}
+
 	}
+
 }
