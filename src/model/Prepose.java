6df4a4b989bc8ce96c29d9901e83adc8996e94bb
Brandon <bblanc030509@gmail.com>
2024-11-25 17:33:49 -0500

    fixed prepose class

--- src/model/Prepose.java
+++ src/model/Prepose.java
@@ -4,120 +4,22 @@ import java.io.Serializable;
 
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
-public class Prepose implements Serializable {
+public class Prepose extends Personne implements Serializable {
 	private static final long serialVersionUID = -5296672519460246263L;
 
 	//Instances
-	private SimpleIntegerProperty numEmp;
-	private SimpleStringProperty nom;
-	private SimpleStringProperty prenom;
-	private SimpleStringProperty adresse;
-	private SimpleStringProperty tel;
-	private SimpleStringProperty password;
+	//private SimpleIntegerProperty numEmp;
+	private StringProperty nom;
+	private StringProperty prenom;
+	private StringProperty adresse;
+	private StringProperty tel;
+	private StringProperty password;
 	
 	
-	//Constucteur
-	public Prepose(SimpleStringProperty nom, SimpleStringProperty prenom, SimpleStringProperty adresse,
-			SimpleStringProperty tel, SimpleStringProperty password) {
-		this.nom = nom;
-		this.prenom = prenom;
-		this.adresse = adresse;
-		this.tel = tel;
-		this.password = password;
+	public Prepose(String nom, String prenom, String adresse, String telephone) {
+		super(nom, prenom, adresse, telephone);
 	}
-
-
-	
-	//Getters et Setters
-	public SimpleIntegerProperty getNumEmpProperty() {
-		return numEmp;
-	}
-	
-	public int getNumEmp() {
-		return numEmp.get();
-	}
-
-
-	public void setNumEmp(SimpleIntegerProperty numEmp) {
-		this.numEmp = numEmp;
-	}
-
-
-	public SimpleStringProperty getNomProperty() {
-		return nom;
-	}
-	
-	public String getNom() {
-		return nom.get();
-	}
-
-
-	public void setNom(SimpleStringProperty nom) {
-		this.nom = nom;
-	}
-
-
-	public SimpleStringProperty getPrenomProperty() {
-		return prenom;
-	}
-	
-	public String getPrenom() {
-		return prenom.get();
-	}
-
-
-	public void setPrenom(SimpleStringProperty prenom) {
-		this.prenom = prenom;
-	}
-
-
-	public SimpleStringProperty getAdresseProperty() {
-		return adresse;
-	}
-	
-	public String getAdresse() {
-		return adresse.get();
-	}
-
-
-	public void setAdresse(SimpleStringProperty adresse) {
-		this.adresse = adresse;
-	}
-
-
-	public SimpleStringProperty getTelProperty() {
-		return tel;
-	}
-	
-	public String getTel() {
-		return tel.get();
-	}
-
-
-	public void setTel(SimpleStringProperty tel) {
-		this.tel = tel;
-	}
-
-
-	public SimpleStringProperty getPasswordProperty() {
-		return password;
-	}
-	
-	public String getPassword() {
-		return password.get();
-	}
-
-
-	public void setPassword(SimpleStringProperty password) {
-		this.password = password;
-	}
-
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
-	
-	
 	
 }
